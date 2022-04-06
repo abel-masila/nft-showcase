4222b423a7c0a1339bce48f6ec7a9afdc6a04180
Abel Masila <abel@x2ai.com>
2022-04-07 02:07:39 +0300

    Added boilerplate for all components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { Download, Features, SectionWrapper } from './components';
+import assets from './assets';
+import styles from './styles/Global';
 const App = () => {
   return (
     <div className="App">
